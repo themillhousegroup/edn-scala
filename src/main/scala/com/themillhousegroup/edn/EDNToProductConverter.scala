4c0c30dd5222a653fa335ab7d0e280a42effca0a
John Marshall <john.marshall2@auspost.com.au>
2014-08-27 12:44:48 +1000

    Handle Long->Int conversion

--- src/main/scala/com/themillhousegroup/edn/EDNToProductConverter.scala
+++ src/main/scala/com/themillhousegroup/edn/EDNToProductConverter.scala
@@ -24,11 +24,19 @@ object EDNToProductConverter {
     }
 
     println(s"args: ${args.mkString}")
-    targetClass.getConstructors.head.newInstance(args:_*).asInstanceOf[T]
+    targetClass.getConstructors.head.newInstance(args.asInstanceOf[Array[Object]]:_*).asInstanceOf[T]
   }
 
   private[this] def isOption(fieldType:Class[_]) = {
     fieldType.isAssignableFrom(classOf[Option[_]])
+  }
+
+  private[this] def isInt(fieldType:Class[_]) = {
+    fieldType.isAssignableFrom(classOf[Int])
+  }
+
+  private[this] def isJLong(fieldType:Class[_]) = {
+    fieldType.isAssignableFrom(classOf[java.lang.Long])
   }
 
   private def matchOptionalField[F](fieldName:String, fieldType: Class[F], mapValue: Option[AnyRef]):Option[F] = {
@@ -40,13 +48,18 @@ object EDNToProductConverter {
     }
   }
 
-  private def matchRequiredField[F](fieldName:String, fieldType: Class[F], mapValue: Option[AnyRef]):String = {
-    mapValue.fold[String] {
+  private def matchRequiredField[F](fieldName:String, fieldType: Class[F], mapValue: Option[AnyRef]):F = {
+    mapValue.fold[F] {
       // EDN does NOT contain a field with this keyword
       throw new IllegalArgumentException(s"Non-optional field '${fieldName}' was not found in the given EDN.")
     } { v =>
-      // EDN contains a value for this keyword
-      v.asInstanceOf[String]
+      // EDN-Java tends to favour java.lang.Long where a case class would use an Int;
+      // make the conversion transparent:
+      if (isInt(fieldType) && isJLong(v.getClass)) {
+        v.asInstanceOf[Long].toInt.asInstanceOf[F]
+      } else {
+        v.asInstanceOf[F]
+      }
     }
   }
 }
